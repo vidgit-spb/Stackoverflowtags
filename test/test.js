e1589e0986bb39b02f78aca1a4c165518d4ef8b6
Bataliants Aleksandr <bataliants@gmail.com>
2020-02-11 16:50:57 +0300

    change entry point, delete unnecessary files

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 const got = require('got');
-const cur = require('./../src/index.js');
+const server = require('../src/index.js');
 
 describe('return type check', () => {
-    const newInstance = new cur.MyServer(8000);
+    const newInstance = new server.MyServer(8000);
 
-    before(() => {
-        newInstance.startServer();
+    before(async () => {
+        await newInstance.startServer();
     });
 
-    after(() => {
-        newInstance.stopServer();
+    after(async () => {
+        await newInstance.stopServer();
     });
 
     it('Check 200 http and Name', async () => {
@@ -19,7 +19,6 @@ describe('return type check', () => {
         assert.equal(answer.statusCode, 200);
         assert.equal(answer.body.includes('Hello Node.js'), true);
 
-        return newInstance.stopServer();
     });
 
 });
