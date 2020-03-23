b6e8ff50cdf6bd1f5768f22bc17980c5631cebfd
Bataliants Aleksandr <bataliants@gmail.com>
2020-03-23 18:36:44 +0300

    create mock test

--- test/test.js
+++ test/test.js
@@ -1,24 +1,37 @@
 const got = require('got');
 const { expect } = require('chai');
-const server = require('../');
+const nock = require('nock');
 const { default: defaultValues } = require('../lib/server/defaultValues');
 
 describe('return type check', () => {
-    const newInstance = new server.MyServer();
-
-    before(async () => {
-        await newInstance.startServer();
-    });
-
-    after(async () => {
-        await newInstance.stopServer();
-    });
-
     it('Check 200 http and Name', async () => {
+        const firstExpectedId = 59791234;
+        const secondExpectedId = 59744176;
+
+        nock(`http://${defaultValues.address}:${defaultValues.port}`)
+            .get('/')
+            .reply(200, {
+                expected: {
+                    questionId: '59791234',
+                    tags:       'node.js,testing,intellij-idea,jestjs,testcafe',
+                    title:      '	testcafe execution via intelliJ run action'
+                }
+            })
+            .get('/')
+            .reply(200, {
+                expected: {
+                    questionId: '59744176',
+                    tags:       'testing,automation,automated-tests,e2e-testing,testcafe',
+                    title:      'Data Driven test not passing 1 parameter'
+                } });
+
         const answer = await got(`http://${defaultValues.address}:${defaultValues.port}`);
 
-        expect(answer.statusCode).to.equal(200);
-        expect(answer.body).include('Hello Node.js');
+        expect(JSON.parse(answer.body).expected.questionId).to.equal(`${firstExpectedId}`);
+
+        const answer2 = await got(`http://${defaultValues.address}:${defaultValues.port}`);
+
+        expect(JSON.parse(answer2.body).expected.questionId).to.equal(`${secondExpectedId}`);
 
     });
 
