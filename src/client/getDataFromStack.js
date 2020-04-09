348dada3f54e13a307ce16686f52ce49e36f8240
Bataliants Aleksandr <bataliants@gmail.com>
2020-04-09 13:18:46 +0300

    add materialize

--- src/client/getDataFromStack.js
+++ src/client/getDataFromStack.js
@@ -1,29 +1,43 @@
 import URL from '../server/url.js';
 
+
+function wrapStackObject(json){
+    let tableHtml = '';
+    for (const code in json) {
+        let massTags = json[code].tags;
+        let canAdd = '&#10006';
+
+        if(massTags.length == 5){
+            canAdd = '&#10004';
+        }
+        let tagsString = ``;
+        for(const idTag in massTags){
+             tagsString+= `<td class = "chip" style ="text-allign:center">${massTags[idTag]} </td>\n`
+        }
+    
+        tableHtml += `
+           <tr align ="center">
+           <td class = "hoverable" id = ${code}>${code} </td>
+           <td class = "hoverable" id  = "idDataTable">${json[code].question_id} </td>
+           ${tagsString}
+           <td class = "hoverable">${json[code].title} </td>
+           <td class = "hoverable">${canAdd}</td>
+           <tr>
+           `;
+        }
+    return tableHtml;
+}
+
 export async function getData () {
     try {
         const response = await fetch(URL);
         const json = await response.json();
-
-        let tableHtml = '';
-        for (const code in json) {
-            tableHtml += `
-               <tr>
-               <td class = "hoverable">${code} </td>
-               <td class = "hoverable">${json[code].question_id} </td>
-               <td class = "hoverable">${json[code].tags} </td>
-               <td class = "hoverable">${json[code].title} </td>
-               <td class = "hoverable">${canAdd}</td>
-               <tr>
-               `;
-        }
-    
+        console.log(json);
+        wrapStackObject(json);
         const table = document.querySelector('#dataTable');
-
         if (!table)
             throw new Error('Cannot find the root table');
-        
-        table.innerHTML = tableHtml;
+        table.innerHTML = wrapStackObject(json);;
     }
     catch (error) {
         console.log(error);
