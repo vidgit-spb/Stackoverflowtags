e7b7a147880b03196058f1d45ecc93cd2759b966
Bataliants Aleksandr <bataliants@gmail.com>
2020-04-28 13:25:53 +0300

    change chips code to correct visualization

--- src/client/getDataFromStack.js
+++ src/client/getDataFromStack.js
@@ -3,25 +3,28 @@ import URL from '../server/url.js';
 
 function wrapStackObject (json) {
     let tableHtml = '';
-    let tagsString = ``;
 
     for (const code in json) {
+        let tagsString = '<div class="chips chips-initial input-field">';
         const massTags = json[code].tags;
         let canAdd = '&#10006';
 
         if (massTags.length === 5)
             canAdd = '&#10004';
 
-
-        for (const idTag in massTags)
-            tagsString += `<td class = "chip" style ="text-allign:center">${massTags[idTag]} </td>\n`;
-
+        
+        for (const idTag in massTags){
+            tagsString += `<div class="chip" tabindex="0"> ${massTags[idTag]} <i class="material-icons close"> X </i> </div>` 
+        }
+        tagsString +=`<input class="input" id = ${json[code].question_id}> </div>`     
 
         tableHtml += `
            <tr align ="center">
            <td class = "hoverable" id = ${code}>${code} </td>
-           <td class = "hoverable" id  = "idDataTable">${json[code].question_id} </td>
+           <td class = "hoverable" id  = "idDataTable">${json[code].question_id} </td> 
+           <td>
            ${tagsString}
+           </td>
            <td class = "hoverable">${json[code].title} </td>
            <td class = "hoverable">${canAdd}</td>
            <tr>
